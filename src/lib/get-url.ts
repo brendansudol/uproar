cde3d1a70bf9f9f30f2174eb866ae149a60a46e1
brendan sudol <brendansudol@gmail.com>
2025-11-10 00:00:54 -0500

    fix get-url

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -3,9 +3,9 @@ export function getURL(path: string = "") {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== ""
       ? process.env.NEXT_PUBLIC_SITE_URL
-      : // If not set, check for NEXT_PUBLIC_VERCEL_URL, which is automatically set by Vercel.
-      process?.env?.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ""
-      ? process.env.NEXT_PUBLIC_VERCEL_URL
+      : // If not set, check for VERCEL_URL, which is automatically set by Vercel.
+      process?.env?.VERCEL_URL && process.env.VERCEL_URL.trim() !== ""
+      ? process.env.VERCEL_URL
       : // If neither is set, default to localhost for local development.
         "http://localhost:3000/"
 
