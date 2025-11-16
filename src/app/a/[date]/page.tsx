28eb6d926f9a9ef04e811053a2d72842b0082ca7
brendan sudol <brendansudol@gmail.com>
2025-11-15 20:40:26 -0500

    start archive route

---
+++ src/app/a/[date]/page.tsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation"
+import { validateDate } from "@/lib/dates"
+
+type Params = Promise<{ date: string }>
+type Props = { params: Params }
+
+export default async function Page({ params }: Props) {
+  const { date } = await params
+  const dateISO = validateDate(date)
+
+  if (dateISO == null) {
+    return redirect("/")
+  }
+
+  return <div>stay tuned! ({dateISO})</div>
+}
